87aabf818b5fdac8e528e936d6be93ba757f5497
Jeremygaop <jeremygaop@gmail.com>
2023-08-10 15:31:48 +0800

    update

--- step4_model_testing/model_testing.py
+++ step4_model_testing/model_testing.py
@@ -98,6 +98,7 @@ def test_logistic(data, factors, start_date, train_period, test_period, symbol, 
     all_rmse_out = []
     all_hit_ratio_in = []
     all_hit_ratio_out = []
+    y_test_preds = pd.Series()
 
     start_date = start_date
 
@@ -115,12 +116,13 @@ def test_logistic(data, factors, start_date, train_period, test_period, symbol, 
         train_data = data.loc[start_date:train_date]
         train_data = train_data[:-1]
 
-        spread_upper = train_data[filter_factor].quantile(upper_quantile)
-        spread_lower = train_data[filter_factor].quantile(lower_quantile)
+        filtor_upper = train_data[filter_factor].quantile(upper_quantile)
+        filtor_lower = train_data[filter_factor].quantile(lower_quantile)
+
 
         test_data = data.loc[test_date:end_date]
         # select the data with spread which is lager than the upper quantile or lower than the lower quantile
-        test_data = test_data[(test_data[filter_factor] > spread_upper) | (test_data[filter_factor] < spread_lower)]
+        test_data = test_data[(test_data[filter_factor] > filtor_upper) | (test_data[filter_factor] < filtor_lower)]
         test_data = test_data[:-1]
 
         if len(test_data) == 0:
@@ -144,11 +146,14 @@ def test_logistic(data, factors, start_date, train_period, test_period, symbol, 
 
         y_train_pred = model_logistic.predict(X_train)
         y_test_pred = model_logistic.predict(X_test)
+        y_test_pred = pd.Series(y_test_pred, index=y_test.index)
+        y_test_preds = pd.concat([y_test_preds, y_test_pred], axis=0)
 
         r2_in = model_logistic.score(X_train, y_train)
         r2_out = model_logistic.score(X_test, y_test)
         rmse_in = np.sqrt(np.mean((model_logistic.predict(X_train) - y_train) ** 2))
         rmse_out = np.sqrt(np.mean((model_logistic.predict(X_test) - y_test) ** 2))
+
 
         # calculate hit ratio
         hit_ratio_in = 0
@@ -171,24 +176,32 @@ def test_logistic(data, factors, start_date, train_period, test_period, symbol, 
 
         start_date = valid_date_n_days_later(start_date, data, 1)
 
-    # plot the 4 lists in the same plot
-    plt.plot(all_r2_in, label='in-sample R2')
-    plt.plot(all_r2_out, label='out-sample R2')
-    # plt.plot(all_rmse_in, label='in-sample RMSE')
-    # plt.plot(all_rmse_out, label='out-sample RMSE')
-    plt.plot(all_hit_ratio_in, label='in-sample hit ratio')
-    plt.plot(all_hit_ratio_out, label='out-sample hit ratio')
-    plt.title('Logistic Regression on ' + symbol)
-    plt.legend()
-    plt.show()
+    # # plot the 4 lists in the same plot
+    # plt.plot(all_r2_in, label='in-sample R2')
+    # plt.plot(all_r2_out, label='out-sample R2')
+    # # plt.plot(all_rmse_in, label='in-sample RMSE')
+    # # plt.plot(all_rmse_out, label='out-sample RMSE')
+    # plt.plot(all_hit_ratio_in, label='in-sample hit ratio')
+    # plt.plot(all_hit_ratio_out, label='out-sample hit ratio')
+    # plt.title('Logistic Regression on ' + symbol)
+    # plt.legend()
+    # plt.show()
+    #
+    # # print R2
+    # print("in-sample R2: ", sum(all_r2_in) / len(all_r2_in))
+    # print("out-sample R2: ", sum(all_r2_out) / len(all_r2_out))
+    # print("in-sample RMSE: ", sum(all_rmse_in) / len(all_rmse_in))
+    # print("out-sample RMSE: ", sum(all_rmse_out) / len(all_rmse_out))
+    # print("in-sample hit ratio: ", sum(all_hit_ratio_in) / len(all_hit_ratio_in))
+    # print("out-sample hit ratio: ", sum(all_hit_ratio_out) / len(all_hit_ratio_out))
 
-    # print R2
-    print("in-sample R2: ", sum(all_r2_in) / len(all_r2_in))
-    print("out-sample R2: ", sum(all_r2_out) / len(all_r2_out))
-    print("in-sample RMSE: ", sum(all_rmse_in) / len(all_rmse_in))
-    print("out-sample RMSE: ", sum(all_rmse_out) / len(all_rmse_out))
-    print("in-sample hit ratio: ", sum(all_hit_ratio_in) / len(all_hit_ratio_in))
-    print("out-sample hit ratio: ", sum(all_hit_ratio_out) / len(all_hit_ratio_out))
+    # set the name of y_test_preds as signal
+    y_test_preds = pd.DataFrame(y_test_preds, columns=['signal'])
+
+    data = pd.merge(data, y_test_preds, left_index=True, right_index=True, how='left')
+
+    data = data[['spread', 'signal']]
+    return data
 
 
 def valid_date_n_days_later(start, all_data, n):
